21b0e635b681f71d356dd9b49bff26120bc3c14f
AntonLabkovich <sharcnatch@gmail.com>
2020-09-27 23:53:08 +0300

    init

---
+++ pages/auctions/index.tsx
@@ -0,0 +1,5 @@
+const Auctions = () => {
+  return <h1>аукционы</h1>;
+};
+
+export default Auctions;
---
+++ pages/profile/index.tsx
@@ -0,0 +1,5 @@
+const Profile = () => {
+  return <h1>профиль юзера</h1>;
+};
+
+export default Profile;
